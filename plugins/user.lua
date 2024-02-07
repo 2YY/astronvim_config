c5cb8aa64dd753b0fdfe94aa0f615ae71257f519
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-02-07 19:55:21 +0900

    feat: Neogen 追加

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,6 +10,18 @@ return {
   --   end,
   -- },
   {
+    "danymat/neogen",
+    cmd = "Neogen",
+    opts = {
+      snippet_engine = "luasnip",
+      languages = {
+        lua = { template = { annotation_convention = "emmylua" } },
+        typescript = { template = { annotation_convention = "tsdoc" } },
+        typescriptreact = { template = { annotation_convention = "tsdoc" } },
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require "cmp"

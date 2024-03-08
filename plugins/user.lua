9925c216133ae24aa51eee118414fa2cd7d1c2f2
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-03-08 19:16:00 +0900

    feat: Neogenの設定を別ファイルに分離し、キーマップの設定も追加

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,18 +10,6 @@ return {
   --   end,
   -- },
   {
-    "danymat/neogen",
-    cmd = "Neogen",
-    opts = {
-      snippet_engine = "luasnip",
-      languages = {
-        lua = { template = { annotation_convention = "ldoc" } },
-        typescript = { template = { annotation_convention = "tsdoc" } },
-        typescriptreact = { template = { annotation_convention = "tsdoc" } },
-      },
-    },
-  },
-  {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require "cmp"

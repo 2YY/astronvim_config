83464be1e24220d7f9295119edb879738178e4dd
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-03-08 19:09:00 +0900

    feat: Luaのドキュメント方式をldocに変更

--- plugins/user.lua
+++ plugins/user.lua
@@ -15,7 +15,7 @@ return {
     opts = {
       snippet_engine = "luasnip",
       languages = {
-        lua = { template = { annotation_convention = "emmylua" } },
+        lua = { template = { annotation_convention = "ldoc" } },
         typescript = { template = { annotation_convention = "tsdoc" } },
         typescriptreact = { template = { annotation_convention = "tsdoc" } },
       },

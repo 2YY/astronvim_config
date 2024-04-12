a13850fdf2072134576e1039ce5bd7021e91c4e4
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-04-13 01:54:25 +0900

    feat: タイポ修正

--- lua/community.lua
+++ lua/community.lua
@@ -16,7 +16,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { improt = "astrocommunity.pack.typescript"},
+  { import = "astrocommunity.pack.typescript"},
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },

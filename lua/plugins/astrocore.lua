6bc9a7a5923a34b79e94b5b845a1c0fe4b837bc7
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-04-26 21:31:25 +0900

    feat: Emmetのキーマップ設定

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -35,6 +35,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        user_emmet_expandabbr_key = "<Tab>",
       },
     },
     -- Mappings can be configured through AstroCore as well.

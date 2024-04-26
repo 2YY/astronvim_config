62a7b2f29a62d0d07f8fda7456539f7d2a1e3b65
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-04-26 21:27:22 +0900

    feat: mason-null-ls のパーサーを断捨離

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -22,10 +22,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "cspell",
         "eslint_d",
-        "prettier",
-        "rustfmt",
         -- add more arguments for adding more null-ls sources
       })
     end,
